aab23d8ffbee45975da597097e143b5aeb6edcb6
Christopher <20220207@itla.edu.do>
2024-07-13 01:04:31 -0400

    Creacion de la interfaz del dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+      <script defer src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
